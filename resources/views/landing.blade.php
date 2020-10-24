3dbb8b332b3d8b9f147ea70d1a8553439c0fc345
valfeclone <fadhlanpasyah13@gmail.com>
2020-10-24 21:44:06 +0700

    acong mau page landing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,2 @@
+<title>Acong Ganteng</title>
+<h1>ACONG CINA BANYAK MAUNYA BGST :(</h1>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('landing');
 });
 
 Route::group([ "middleware" => ['auth:sanctum', 'verified'] ], function() {
