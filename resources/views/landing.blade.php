48d7e95bd90f5cc6ba308983657bc73f5b2b191b
Mario Adriel <marioadrieel@gmail.com>
2020-10-26 21:58:10 +0700

    Set up the SASS environment

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,2 +1,18 @@
 <title>Acong Ganteng</title>
-<h1>ACONG CINA BANYAK MAUNYA BGST :(</h1>
+
+<div class="landing">
+    <div class="navbar">
+        <a class="navbar__logo-box">
+            <img src="/ajisaka-logo.png" alt="" class="navbar__logo">
+        </a>
+        <div class="navbar__links">
+            <a href="" class="navbar__link">About</a>
+            <a href="" class="navbar__link">Matlom</a>
+            <a href="" class="navbar__link">Events</a>
+            <a href="" class="navbar__link">Contact</a>
+        </div>
+        <div class="navbar__util">
+            <a href="" class="navbar__link">Login</a>
+        </div>
+    </div>
+</div>
