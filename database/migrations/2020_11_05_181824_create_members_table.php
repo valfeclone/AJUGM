474a721d48c1e296ede4355ecd26d2c86907495c
valfeclone <fadhlanpasyah13@gmail.com>
2020-11-06 02:08:00 +0700

    nambah table member

---
+++ database/migrations/2020_11_05_181824_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('fakultas');
+            $table->string('jurusan');
+            $table->string('path_foto_ktm');
+            $table->string('email')->unique();
+            $table->string('linkedin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
