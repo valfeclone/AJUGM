ced5e91f29dcc7518c134794bbb19005e1990453
Mario Adriel <marioadrieel@gmail.com>
2020-12-01 19:29:04 +0700

    Add and style creative market tenant page

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -140,7 +140,34 @@ $(document).ready(() => {
             $('.register__geometry--circle').addClass('show');
         }
     })
+
+    
 });
+
+// Carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Thumbnail image controls
+function currentSlide(n, carousel) {
+    showSlides(slideIndex = n, carousel);
+}
+
+function showSlides(n, carousel) {
+    var i;
+    var slides = document.getElementsByClassName(`tenant-product--${carousel}__image`);
+    var dots = document.getElementsByClassName(`tenant-product--${carousel}__dot`);
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
 
 $(document).click(function(event){
     var $trigger = $(".dropdown");
