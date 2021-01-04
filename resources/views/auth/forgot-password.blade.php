5826d40d1aba607dd51d330bf88a797182aca605
farissatyaw <redcolony14@gmail.com>
2021-01-04 14:30:43 +0700

    Forgot Password and register

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="/images/ajisaka-logo.png" alt="" class="logo">
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            <p style="text-align: justify;">Jika lupa dengan passwordmu, tenang saja. Tulis di kolom dibawah email team mu, dan kita akan mengirimkan email berisikan link reset password untuk mengubah passwordmu</p>
         </div>
 
         @if (session('status'))
