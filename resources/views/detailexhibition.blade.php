c892b5b8b384c3bf560c269d37ae8ee0d8b4db8d
farissatyaw <redcolony14@gmail.com>
2021-04-10 08:28:01 +0700

    Add 3d exhibition

---
+++ resources/views/detailexhibition.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        Ajisaka | 3D Exhibition
+    </title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/frontend.js') }}"></script>
+    <script src="{{ asset('js/validation.js') }}"></script>
+</head>
+<body>
+    <div class="app">
+        <nav class="header">
+            <a href="/" class="logo-box">
+                <img src="/images/ajisaka-logo.png" alt="" class="logo">
+            </a>
+            <div class="header__link-box">
+                <a href="/about" class="link-item"><p class="text-bold text-black text-uppercase">About</p></a>
+                <div class="dropdown">
+                    <button class="dropdown-button--hover"><p class="text-bold text-black text-uppercase">Kompetisi</p></a>
+                    <div class="dropdown-menu">
+                        <div class="dropdown-section">
+                            <a href="/kompetisi/arjuna" class="dropdown-option">Arjuna</a>
+                            <a href="/kompetisi/kresna" class="dropdown-option">Kresna</a>
+                            <a href="/kompetisi/nakula" class="dropdown-option">Nakula</a>
+                            <a href="/kompetisi/prahasta" class="dropdown-option">Prahasta</a>
+                            <a href="/kompetisi/sadewa" class="dropdown-option">Sadewa</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <button class="dropdown-button--hover"><p class="text-bold text-black text-uppercase">Events</p></a>
+                    <div class="dropdown-menu">
+                        <div class="dropdown-section">
+                            <a href="/creativemarket" class="dropdown-option">Creative Market</a>
+                            <!-- <a href="/webinar" class="dropdown-option">Webinar</a> -->
+                            <a href="/exhibition" class="dropdown-option">3D Exhibition</a>
+                        </div>
+                    </div>
+                </div>
+                <a href="/contact" class="link-item"><p class="text-bold text-black text-uppercase">FAQ & Contact</p></a>
+            </div>
+            <div class="header__util">
+                @guest
+                <a href="/login" class="link-item"><p class="text-bold text-black text-uppercase">Login</p></a>
+                @else
+                <div class="dropdown">
+                    <button class="dropdown-button"><p class="text-bold text-black text-uppercase">{{auth()->User()->name}}</p></a>
+                    <div class="dropdown-menu">
+                        <div class="dropdown-section">
+                            <a href="/tim/update" class="dropdown-option">Account</a>
+                            <a href="/tim/member" class="dropdown-option">Team Members</a>
+                        </div>
+                        <div class="dropdown-section">
+                            <a href="/tim/uploadbukti" class="dropdown-option">Bukti Pembayaran</a>
+                            <a href="/tim/uploadkarya" class="dropdown-option">Upload Karya</a>
+                        </div>
+                        <div class="dropdown-section">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <input type="submit" class="dropdown-option" value="Log out">
+                            </form>          
+                        </div>
+                    </div>
+                </div>
+                @endguest
+            </div>
+
+            <div class="header__link-box--responsive">
+                <a href="/about" class="link-item"><p class="text-bold text-black text-uppercase">About</p></a>
+                <a href="/contact" class="link-item"><p class="text-bold text-black text-uppercase">FAQ & Contact</p></a>
+                <div class="link-item nested-menu">
+                    <p class="text-bold text-black text-uppercase">Kompetisi <i class="fas fa-angle-down nested-menu__arrow"></i></p>
+                    <div class="menu">
+                        <a href="/kompetisi/arjuna" class="link-item"><p class="text-bold text-black text-uppercase">Arjuna</p></a>
+                        <a href="/kompetisi/kresna" class="link-item"><p class="text-bold text-black text-uppercase">Kresna</p></a>
+                        <a href="/kompetisi/nakula" class="link-item"><p class="text-bold text-black text-uppercase">Nakula</p></a>
+                        <a href="/kompetisi/prahasta" class="link-item"><p class="text-bold text-black text-uppercase">Prahasta</p></a>
+                        <a href="/kompetisi/sadewa" class="link-item"><p class="text-bold text-black text-uppercase">Sadewa</p></a>
+                    </div>
+                </div>
+                <div class="link-item nested-menu">
+                    <p class="text-bold text-black text-uppercase">Events <i class="fas fa-angle-down nested-menu__arrow"></i></p>
+                    <div class="menu">
+                        <a href="/creativemarket" class="link-item"><p class="text-bold text-black text-uppercase">Creative Market</p></a>
+                        <!-- <a href="/webinar" class="link-item"><p class="text-bold text-black text-uppercase">Webinar</p></a> -->
+                        <a href="/exhibition" class="link-item"><p class="text-bold text-black text-uppercase">3D Exhibition</p></a>
+                    </div>
+                </div>
+                <div class="hr"></div>
+                @guest
+                <a href="/login" class="link-item"><p class="text-bold text-black text-uppercase">Login</p></a>
+                @else
+                <a href="/tim/update" class="link-item"><p class="text-bold text-black text-uppercase">{{auth()->User()->name}}'s Dashboard</p></a>
+                <form action="/logout" method="POST">
+                    @csrf
+                    <input type="submit" class="link-item text-bold text-black text-uppercase no-border" value="Log out">
+                </form> 
+                @endguest
+
+                <i class="fas fa-times-circle close-button"></i>
+            </div>
+            <a href="javascript:void(0);" class="icon header__hamburger-button">
+                <i class="fa fa-bars header__hamburger text-black"></i>
+            </a>
+        </nav>
+
+        <iframe src="https://i.simmer.io/@AJISAKAUGM/ajisaka-online-exhibition" style="width:100%;height:600px"></iframe>
+        <footer class="footer">
+            <a href="/" class="logo-box">
+                <img src="/images/ajisaka-logo.png" alt="" class="logo">
+            </a>
+            <div class="footer__link-box">
+                <a href="/about" class="link-item"><p class="text-bold text-grey text-uppercase">About</p></a>
+                <a href="/#landing-matlom" class="link-item"><p class="text-bold text-grey text-uppercase">Kompetisi</p></a>
+                <a href="/#" class="link-item"><p class="text-bold text-grey text-uppercase">Events</p></a>
+                <a href="/contact" class="link-item"><p class="text-bold text-grey text-uppercase">FAQ & Contact</p></a>
+            </div>
+            <div class="footer__util">
+                <p class="text-regular text-grey">&#169;2020-Form | All Rights Reserved</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MemberController;
-use App\Http\Controllers\UploadbuktiController;
-use App\Http\Controllers\UploadFileLombaController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UMKMController;
-use App\Models\umkm;
+use App\Http\Controllers\UploadbuktiController;
+use App\Http\Controllers\UploadFileLombaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,35 +24,34 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-
-Route::get('/', [ UMKMController::class, "indexlanding" ]);
+Route::get('/', [UMKMController::class, 'indexlanding']);
 
 //akses dashboard admin, harus pake admin ya hehehe
-Route::group([ "middleware" => ['auth:admins', 'verified'] ], function () {
-    Route::view('/admin/dashboard', "dashboard")->name('admin.dashboard');
-    Route::get('/user', [ UserController::class, "index_view" ])->name('user');
-    Route::get('/member', [ MemberController::class, "index_view" ])->name('member');
-    Route::view('/user/new', "pages.user.user-new")->name('user.new');
-    Route::view('/user/edit/{userId}', "pages.user.user-edit")->name('user.edit');
-    Route::post('/admin/verify/{id}', [ UserController::class, "verify_pembayaran" ]);
+Route::group(['middleware' => ['auth:admins', 'verified']], function () {
+    Route::view('/admin/dashboard', 'dashboard')->name('admin.dashboard');
+    Route::get('/user', [UserController::class, 'index_view'])->name('user');
+    Route::get('/member', [MemberController::class, 'index_view'])->name('member');
+    Route::view('/user/new', 'pages.user.user-new')->name('user.new');
+    Route::view('/user/edit/{userId}', 'pages.user.user-edit')->name('user.edit');
+    Route::post('/admin/verify/{id}', [UserController::class, 'verify_pembayaran']);
 
     //akses gambar bukti
-    Route::get('/images/bukti/{file}', [ function ($file) {
+    Route::get('/images/bukti/{file}', [function ($file) {
         $path = storage_path('app/bukti_pembayaran/'.$file);
 
         if (file_exists($path)) {
-            return response()->file($path, array('Content-Type' =>'image/jpeg'));
+            return response()->file($path, ['Content-Type' => 'image/jpeg']);
         }
 
         abort(404);
     }]);
 
     //akses file lomba
-    Route::get('/images/filelomba/{file}', [ function ($file) {
+    Route::get('/images/filelomba/{file}', [function ($file) {
         $path = storage_path('app/file_lomba/'.$file);
 
         if (file_exists($path)) {
-            return response()->file($path, array('Content-Type' =>'image/jpeg'));
+            return response()->file($path, ['Content-Type' => 'image/jpeg']);
         }
 
         abort(404);
@@ -62,58 +60,61 @@ Route::group([ "middleware" => ['auth:admins', 'verified'] ], function () {
 
 //buat login admin
 Route::get('/admin/login', function () {
-    return view("login_admin");
+    return view('login_admin');
 })->name('admin.login');
-Route::post('/admin/login', [ AdminController::class, "handleLogin" ])
+Route::post('/admin/login', [AdminController::class, 'handleLogin'])
     ->middleware(['guest']);
 
 // buat upload file gambar lomba
-Route::group([ "middleware" => ['auth:sanctum', 'verified'] ], function () {
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     //buat upload bukti bayar
-    Route::get('/tim/uploadbukti', [ UploadbuktiController::class, "upload" ]);
-    Route::post('/tim/uploadbukti', [ UploadbuktiController::class, "proses_upload" ]);
+    Route::get('/tim/uploadbukti', [UploadbuktiController::class, 'upload']);
+    Route::post('/tim/uploadbukti', [UploadbuktiController::class, 'proses_upload']);
 
     //buat upload file lomba
-    Route::get('/tim/uploadkarya', [ UploadFileLombaController::class, "upload" ]);
-    Route::post('/tim/uploadkarya', [ UploadFileLombaController::class, "proses_upload" ]);
-    Route::post('/tim/uploadlink/proses', [ UploadFileLombaController::class, "proses_upload_link" ]);
+    Route::get('/tim/uploadkarya', [UploadFileLombaController::class, 'upload']);
+    Route::post('/tim/uploadkarya', [UploadFileLombaController::class, 'proses_upload']);
+    Route::post('/tim/uploadlink/proses', [UploadFileLombaController::class, 'proses_upload_link']);
 
     //update detail tim
-    Route::get('/tim/update', [ UserController::class, "show_account"]);
-    Route::post('/tim/update', [ UserController::class, "updateUser"]);
-    
+    Route::get('/tim/update', [UserController::class, 'show_account']);
+    Route::post('/tim/update', [UserController::class, 'updateUser']);
+
     //member tim
     Route::get('/tim/member', function () {
-        return view("dashboard_peserta.member");
+        return view('dashboard_peserta.member');
     });
-    Route::get('/tim/member', [ UserController::class, "show_member"]);
-    Route::post('/tim/member', [ MemberController::class, "updateMember"]);
+    Route::get('/tim/member', [UserController::class, 'show_member']);
+    Route::post('/tim/member', [MemberController::class, 'updateMember']);
 });
 
 //artstep
 Route::get('/galeri', function () {
-    return view("artstep");
+    return view('artstep');
 });
 
 //buat register
-Route::post('/register', [ RegisterController::class, "validator" ])
+Route::post('/register', [RegisterController::class, 'validator'])
     ->middleware(['guest']);
 
 //buat creative market
-Route::get('/creativemarket', [ UMKMController::class, "index" ]);
-Route::get('/creativemarket/{umkm}', [ UMKMController::class, "show" ]);
+Route::get('/creativemarket', [UMKMController::class, 'index']);
+Route::get('/creativemarket/{umkm}', [UMKMController::class, 'show']);
 
 Route::get('/exhibition', function () {
-    return view("virtual_museum");
+    return view('virtual_museum');
+});
+Route::get('/detailexhibition', function () {
+    return view('detailexhibition');
 });
 
 //kategori lomba
-Route::view('/kompetisi/arjuna', "kategori.arjuna");
-Route::view('/kompetisi/kresna', "kategori.kresna");
-Route::view('/kompetisi/nakula', "kategori.nakula");
-Route::view('/kompetisi/prahasta', "kategori.prahasta");
-Route::view('/kompetisi/sadewa', "kategori.sadewa");
+Route::view('/kompetisi/arjuna', 'kategori.arjuna');
+Route::view('/kompetisi/kresna', 'kategori.kresna');
+Route::view('/kompetisi/nakula', 'kategori.nakula');
+Route::view('/kompetisi/prahasta', 'kategori.prahasta');
+Route::view('/kompetisi/sadewa', 'kategori.sadewa');
 
-Route::view('/about', "about");
-Route::view('/contact', "contact");
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
 //Route::view('/webinar', "webinar");
